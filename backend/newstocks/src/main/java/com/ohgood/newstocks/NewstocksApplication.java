6fbfbaf6873eb80f87347b4aedcbe8eceabd30a7
1004joonsuk <1004joonsuk@naver.com>
2023-08-31 16:39:26 +0900

    :tada: init: 프로젝트 시작

---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/NewstocksApplication.java
@@ -0,0 +1,14 @@
+package com.ohgood.newstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class NewstocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstocksApplication.class, args);
+	}
+
+}
