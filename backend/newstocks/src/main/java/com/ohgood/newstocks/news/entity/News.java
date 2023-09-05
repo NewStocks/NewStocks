393c01e0e0f7856544e7e44142c1e11291d42b68
OhSeongRak <osr0907@naver.com>
2023-09-05 09:00:25 +0900

    :sparkles: feat: News Entity 생성

---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/news/entity/News.java
@@ -0,0 +1,33 @@
+package com.ohgood.newstocks.news.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class News {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private LocalDateTime publishTime;
+}
