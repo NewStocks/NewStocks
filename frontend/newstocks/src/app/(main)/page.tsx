4253a864442038ddc6ada5c17955cd10f3506304
paengjiwoo <aprilzu0427@gmail.com>
2023-09-05 16:46:20 +0900

    :lipstick: style: community.module.css 생성 및 globals.css 적용

--- frontend/newstocks/src/app/(main)/page.tsx
+++ frontend/newstocks/src/app/(main)/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import '../globals.css';
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}></main>
+    <main className={styles.main}>여기가 메인페이지 입니다</main>
   )
 }
--- frontend/newstocks/src/app/community/page.tsx
+++ frontend/newstocks/src/app/community/page.tsx
@@ -1,7 +1,8 @@
+import '../globals.css';
+import styles from './community.module.css';
+
 export default function CommunityPage() {
   return (
-    <>
-      <h1>Community Page</h1>
-    </>
+    <h1 className={styles.main}>Community Page</h1>
   )
 }
