d96066e5d8df629511c8891c4d24c623ff14b2b5
paengjiwoo <aprilzu0427@gmail.com>
2023-09-05 16:10:13 +0900

    :lipstick: style: layout에서 font 수정

--- frontend/newstocks/src/app/(main)/layout.tsx
+++ frontend/newstocks/src/app/(main)/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const sans = Open_Sans({ subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- frontend/newstocks/src/app/community/layout.tsx
+++ frontend/newstocks/src/app/community/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const sans = Open_Sans({ subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function CommunityLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={sans.className}>
         <nav>여기가 최상단 nav</nav>
         <p>여기는 커뮤니티</p>
         {children}
