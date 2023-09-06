7d220825e0640a0c0288ce220c59953715c339ed
paengjiwoo <aprilzu0427@gmail.com>
2023-09-06 13:04:11 +0900

    :sparkles: feat: CommunityNav 컴포넌트 생성

---
+++ frontend/newstocks/src/components/CommunityNav/CommunityNav.tsx
@@ -0,0 +1,11 @@
+'use client';
+import styles from './CommunityNav.module.css';
+import Link from 'next/link';
+
+export default function CommunityNav() {
+  return(
+    <div>
+      CommunityNav
+    </div>
+  )
+}
