bf2dc0d74ad315ff3d701b853f1086eea2c65b23
김민석 <kmin9811@naver.com>
2023-09-07 15:34:54 +0900

    :sparkles: feat: Member, Stock Mapper 및 DTO 생성

---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/member/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.ohgood.newstocks.member.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private String name;
+    private String profileImage;
+    private String socialId;
+
+
+}
---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/reviewnote/dto/ReviewNoteImageDto.java
@@ -0,0 +1,12 @@
+package com.ohgood.newstocks.reviewnote.dto;
+
+import lombok.Data;
+
+@Data
+public class ReviewNoteImageDto {
+
+    private Long id;
+    private String url;
+
+
+}
