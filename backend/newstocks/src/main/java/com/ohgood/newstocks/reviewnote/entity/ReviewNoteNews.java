5ce4c13098c0617f24ae5ad1c1979fad15939fb7
김민석 <kmin9811@naver.com>
2023-09-07 17:26:49 +0900

    :sparkles: feat: ReviewNoteNews Entity 생성

--- backend/newstocks/src/main/java/com/ohgood/newstocks/reviewnote/entity/ReviewNoteNews.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/reviewnote/entity/ReviewNoteNews.java
@@ -1,4 +1,27 @@
 package com.ohgood.newstocks.reviewnote.entity;
 
+import com.ohgood.newstocks.news.entity.News;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReviewNoteNews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_note_id")
+    private ReviewNote reviewNote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "news_id")
+    private News news;
+
+
 }
