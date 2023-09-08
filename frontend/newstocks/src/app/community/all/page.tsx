00f5c37bf6f7a4bf42cfa8379b9524d134790dab
paengjiwoo <aprilzu0427@gmail.com>
2023-09-08 12:48:12 +0900

    :lipstick: style: 전체노트 페이지 스타일링

--- frontend/newstocks/src/app/community/all/page.tsx
+++ frontend/newstocks/src/app/community/all/page.tsx
@@ -1,9 +1,19 @@
 import styles from './allpage.module.css'
 
+import Button from '@/components/Button/Button'
+
 export default function AllnotesPage() {
   return (
     <div className={styles.main}>
-    <h1>전체 노트</h1>
+      <div className={styles["upper-title"]}>커뮤니티</div>
+      <div className={styles["page-title"]}>전체노트</div>
+        <div className={styles["all-button-box"]}>
+          <div className={styles["sort-button-box"]}>
+            <div className={styles["sorted-Button-width"]}><Button text="🔥인기노트" kindof="sorted"></Button></div>
+            <div className={styles["sorted-Button-width"]}><Button text=" 🔎 종목선택" kindof="sorted"></Button></div>
+          </div>
+          <div className={styles["sorted-Button-width"]} id={styles["ordered"]}><Button text="최신순" kindof="sorted"></Button></div>
+        </div>
     </div>
   )
 }
