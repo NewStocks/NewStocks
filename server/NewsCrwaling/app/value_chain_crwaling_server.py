3f04591bfd16966ab9c2816a8b23d259fd96992f
OhSeongRak <osr0907@naver.com>
2023-09-11 15:04:52 +0900

    :sparkles: feat: 밸류체인 크롤링  서버 완료

---
+++ server/NewsCrwaling/app/value_chain_crwaling_server.py
@@ -0,0 +1,163 @@
+import pymysql, os, requests, time
+from flask import Flask
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from webdriver_manager.chrome import ChromeDriverManager
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+app = Flask(__name__)
+load_dotenv()
+
+# MySQL 연결 설정
+db_config = {
+    "host": os.getenv("DB_URL"),
+    "user": os.getenv("DB_ID"),
+    "password": os.getenv("DB_PWD"),
+    "database": os.getenv("DB_NAME"),
+}
+
+URL = "https://stockrow.com/"
+# 현재 날짜와 시간 가져오기
+current_datetime = datetime.now()
+# 현재 날짜에서 1일을 빼서 하루 전 날짜 얻기
+one_day_ago = current_datetime - timedelta(days=1)
+start_time, end, cnt, driver = 0, 0, 0, 0
+urls, infos = [], []
+months = {
+    "Jan": "-01-",
+    "Feb": "-02-",
+    "Mar": "-03-",
+    "Apr": "-04-",
+    "May": "-05-",
+    "Jun": "-06-",
+    "Jul": "-07-",
+    "Aug": "-08-",
+    "Sep": "-09-",
+    "Oct": "-10-",
+    "Nov": "-11-",
+    "Dec": "-12-",
+}
+
+
+def set_chromedriver():
+    options = webdriver.ChromeOptions()  # 크롬 옵션 객체 생성
+    options.add_argument("disable-gpu")
+    try:
+        # 크롬 드라이버 설치
+        return webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+    except Exception as e:
+        print("예외가 발생했습니다:", str(e))
+        print("외부 크롬드라이버를 다운로드 실패.")
+        raise e
+
+
+def convert_date(date, time_):
+    year = str(datetime.now().year)
+    month = months[date.split()[1]]
+    day = date.split()[0]
+    if len(day) == 1:
+        day = "0" + day
+
+    hour = time_.split(":")[0]
+    minute = time_.split(":")[1][:-2]
+    if time_[-2:] == "PM" and hour != "12":
+        hour = int(hour) + 12
+
+    return year + month + day + " " + str(hour) + ":" + minute
+
+
+def crawler(code, name):
+    global infos, driver
+
+    url = URL + code
+    print(url)
+    driver.get(url)
+    driver.implicitly_wait(20)
+    time.sleep(3)
+
+    single_news_group = driver.find_elements(By.CLASS_NAME, "single-news-group")
+    for sng in single_news_group:
+        groupDate = sng.find_element(By.CLASS_NAME, "group-date")
+        times = sng.find_elements(By.CLASS_NAME, "published-at")
+        titles = sng.find_elements(By.CLASS_NAME, "news-title")
+        companys = sng.find_elements(By.CLASS_NAME, "source")
+
+        for title, time_, company in zip(titles, times, companys):
+            date = convert_date(groupDate.text, time_.text)
+            infos.append(
+                [
+                    code,
+                    title.text,
+                    company.text,
+                    date,
+                    title.get_attribute("href"),
+                    name,
+                ]
+            )
+
+    return
+
+
+@app.route("/save-value-chain-news", methods=["POST"])
+def save_news():
+    global infos, driver
+
+    try:
+        # MySQL 연결
+        conn = pymysql.connect(**db_config)
+        cursor = conn.cursor()
+        driver = set_chromedriver()
+
+        # 밸류체인 명 가져오기
+        cursor.execute("SELECT id, value_chain_name FROM value_chain")
+        lst = cursor.fetchall()
+        codes = [(k[0], k[1]) for k in lst]
+        conn.commit()
+
+        for code, name in codes:
+            crawler(code, name)
+
+        print("뉴스 개수: ", len(infos))
+        cursor.execute("DELETE FROM value_chain_news")
+
+        # 데이터프레임을 MySQL 테이블에 저장
+        for (
+            value_chain_id,
+            title,
+            company,
+            publish_time,
+            url,
+            value_chain_name,
+        ) in infos:
+            insert_query = """
+            INSERT INTO value_chain_news
+            (item_code, title, company, publish_time, url, value_chain_name)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(
+                insert_query,
+                (value_chain_id, title, company, publish_time, url, value_chain_name),
+            )
+            conn.commit()
+
+        print("실행 시간: ", time.time() - start_time)
+        return "데이터가 MySQL에 저장되었습니다."
+
+    except Exception as e:
+        return f"오류 발생: {str(e)}"
+
+    finally:
+        driver.quit()
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print("start_time time:", start_time)
+    app.run(port=5001)
