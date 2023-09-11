b3439fceb4f04718c0c6da6d4e2725ff98f7ea69
paengjiwoo <aprilzu0427@gmail.com>
2023-09-11 16:23:08 +0900

    :lipstick: style: Card component display 속성 및 margin 수정

--- frontend/newstocks/src/app/community/[id]/page.tsx
+++ frontend/newstocks/src/app/community/[id]/page.tsx
@@ -12,8 +12,6 @@ type Props = {
 export default function DetailnotePage({ params, searchParams, }: Props) {
   return (
     <div className={styles.main}>
-      <h1>노트 상세 페이지: {params.id}</h1>
-      <h2>{searchParams.a}</h2>
     </div>
   )
 }
--- frontend/newstocks/src/app/community/all/page.tsx
+++ frontend/newstocks/src/app/community/all/page.tsx
@@ -24,7 +24,6 @@ export default function AllnotesPage() {
         <Card />
         <Card />
         <Card />
-        <Card />
       </div>
     </div>
   )
