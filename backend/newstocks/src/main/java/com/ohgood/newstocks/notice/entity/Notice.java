e4fed81cf366af6679da7f1964e026a7d98f8369
1004joonsuk <1004joonsuk@naver.com>
2023-09-13 13:38:05 +0900

    :sparkles: feat: 공지사항 이미지 엔티티 생성

--- backend/newstocks/src/main/java/com/ohgood/newstocks/notice/entity/Notice.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/notice/entity/Notice.java
@@ -2,18 +2,19 @@ package com.ohgood.newstocks.notice.entity;
 
 import com.ohgood.newstocks.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Entity
-@Table
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice extends BaseTimeEntity {
 
     @Id
-    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,4 +23,8 @@ public class Notice extends BaseTimeEntity {
 
     @Column(length = 3000)
     private String content;
+
+    @OneToMany(mappedBy = "notice", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
+    private List<NoticeImage> noticeImageList;
 }
---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/notice/entity/NoticeImage.java
@@ -0,0 +1,30 @@
+package com.ohgood.newstocks.notice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NoticeImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice_id")
+    private Notice notice;
+}
