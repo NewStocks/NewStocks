201ce00979660e77e1381be9c2f6cc5252e6c693
paengjiwoo <aprilzu0427@gmail.com>
2023-09-15 15:18:41 +0900

    :lipstick: style: Footer 컴포넌트 분리 및 애니메이션 적용

--- frontend/newstocks/src/app/community/page.tsx
+++ frontend/newstocks/src/app/community/page.tsx
@@ -1,31 +1,10 @@
 import styles from './communitypage.module.css';
 
 import LandingView from '@/components/LandingView/LandingView'
+import Footer from '@/components/Footer/Footer'
 import Button from '@/components/Button/Button'
 
 export default function CommunityPage() {
-  // const [isVisible, setIsVisible] = useState(false);
-
-  // useEffect(() => {
-      // 스크롤 이벤트 리스너 추가
-      // const handleScroll = () => {
-      //     const scrollY = window.scrollY;
-      //     // const targetPosition = 100;
-
-      //     if (scrollY >= 0) {
-      //         setIsVisible(true);
-      //     } else {
-      //         setIsVisible(false);
-      //     }
-      // };
-
-      // window.addEventListener('scroll', handleScroll);
-
-      // return () => {
-      //     // window.removeEventListener('scroll', handleScroll);
-      // };
-  // }, []);
-  
 
   return ( 
     <div className={styles.main}>
@@ -46,27 +25,8 @@ export default function CommunityPage() {
 
       <LandingView />  
 
-      <div className={styles["footer"]}>
-        <div className={styles["footer-contents"]}>
-          <div className={styles["footer-box"]}>
-            <div className={styles["footer-title"]}><span>NEWStocks</span></div>
-            <div className={styles["footer-content"]}>오~ 좋은데</div>
-            <div className={styles["footer-content"]}>A210</div>
-          </div>
+      <Footer />
 
-          <div className={styles["footer-box"]}>
-            <div className={styles["footer-title"]}><span>뉴스탁스</span>의 새소식</div>
-            <div className={styles["Button-width"]}><Button text="공지사항" kindof="arrow" highlight={false}></Button></div>
-          </div>
-
-
-          <div>
-            <div className={styles["footer-title"]}>개인정보 보호 및 약관</div>
-            <div className={styles["footer-content"]}>개인정보 처리 방침</div>
-            <div className={styles["footer-content"]}>서비스 이용 약관</div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
