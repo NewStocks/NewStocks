a1b53ac391106381078daeb04111116139c8be87
paengjiwoo <aprilzu0427@gmail.com>
2023-09-15 16:56:21 +0900

    :lipstick: style: 글작성 에디터 스타일 수정

--- frontend/newstocks/src/app/community/create/page.tsx
+++ frontend/newstocks/src/app/community/create/page.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import styles from './createpage.module.css'
+import styled from 'styled-components'
+import Link from 'next/link';
 
 import CreatePostForm from '@/components/CreatePostForm/CreatePostForm'
 
+import { IoIosArrowBack } from 'react-icons/io'
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`
 export default function CreatePage() {
   return (
     <div className={styles["main"]}>
-      <div>
-        <div>뒤로가기</div>
-        <div>오답노트 작성</div>
+      <div className={styles["top-menu"]}>
+        <StyledLink href='/'><div><IoIosArrowBack />뒤로가기</div></StyledLink>
+        <h2>오답노트 작성</h2>
       </div>
 
       <CreatePostForm />
--- frontend/newstocks/src/components/CreatePostForm/CreatePostForm.tsx
+++ frontend/newstocks/src/components/CreatePostForm/CreatePostForm.tsx
@@ -3,6 +3,7 @@ import styles from './createpostform.module.css'
 import { useEffect, useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/theme/toastui-editor-dark.css'
 
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
@@ -17,24 +18,27 @@ export default function CreatePostForm() {
     // 입력창에 입력한 내용을 MarkDown 형태로 취득
     console.log(editorRef.current?.getInstance().getMarkdown());
   };
-
+  
   return (
     <div>
       <Editor
         previewStyle="vertical" // 미리보기 스타일 지정
         height="600px" // 에디터 창 높이
-        initialEditType="hello react editor world!" // 초기 입력모드 설정(디폴트 markdown)
-        useCommandShortcut={true}
-        ref={editorRef}
+        initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+        // placeholder="내용을 입력해주세요!"
+        useCommandShortcut={false}
+        hideModeSwitch={true} 
         toolbarItems={[
           // 툴바 옵션 설정
           ['heading', 'bold', 'italic', 'strike'],
           ['hr', 'quote'],
           ['ul', 'ol', 'task', 'indent', 'outdent'],
-          ['table', 'image', 'link'],
+          ['table', 'link'],
           ['code', 'codeblock']
         ]}
         plugins={[colorSyntax]}
+        theme="dark"
+        ref={editorRef}
       ></Editor>
       <button onClick={handleClick}>지금 작성한 내용 확인해보기</button>
     </div>
