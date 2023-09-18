88bd3071d80bd4f499dfac0ed71e27192a132f08
김민석 <kmin9811@naver.com>
2023-09-18 16:30:48 +0900

    :spakrles: feat: Spring Security Authentication 추가

--- backend/newstocks/src/main/java/com/ohgood/newstocks/auth/jwt/service/JwtServiceImpl.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/auth/jwt/service/JwtServiceImpl.java
@@ -9,12 +9,21 @@ import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Slf4j
 @Service
 public class JwtServiceImpl implements JwtService {
 
@@ -24,6 +33,8 @@ public class JwtServiceImpl implements JwtService {
 
     private static final int ACCESS_TOKEN_EXPIRE_MINUTES = 1;
     private static final int REFRESH_TOKEN_EXPIRE_MINUTES = 2;
+
+    private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
     @Override
     public <T> String createAccessToken(String key, T data) {
@@ -41,9 +52,19 @@ public class JwtServiceImpl implements JwtService {
     public <T> String create(String key, T data, String subject, long expire) {
         Claims claims = Jwts.claims()
             .setSubject(subject)
-            .setIssuedAt(new Date())
+                .setIssuedAt(new Date())
             .setExpiration(new Date(System.currentTimeMillis() + expire));
         claims.put(key, data);
+
+        UserDetails userDetails = User.builder()
+            .username(data.toString())
+            .password(data + SALT)
+            .roles("user")
+            .build();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            userDetails, null, authoritiesMapper.mapAuthorities(userDetails.getAuthorities())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         return Jwts.builder()
             .setHeaderParam("typ", "JWT")
             .setClaims(claims)
