44ae940d57dc29cd2e6b39b80e190683d59fe7db
김민석 <kmin9811@naver.com>
2023-09-18 14:00:46 +0900

    :sparkles: feat: reply insert API created

---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/reviewnote/dto/ReplyReqDto.java
@@ -0,0 +1,9 @@
+package com.ohgood.newstocks.reviewnote.dto;
+
+import lombok.Data;
+
+@Data
+public class ReplyReqDto {
+
+    private String content;
+}
---
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/reviewnote/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package com.ohgood.newstocks.reviewnote.repository;
+
+import com.ohgood.newstocks.reviewnote.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+}
