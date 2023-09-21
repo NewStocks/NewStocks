d788b6f3b512b8d31d6bf65ac6f598dce9cac6ce
김민석 <kmin9811@naver.com>
2023-09-21 13:48:05 +0900

    :spakles: feat: 멤버 프로필 조회 기능 추가

--- backend/newstocks/src/main/java/com/ohgood/newstocks/member/controller/MemberController.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/member/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.ohgood.newstocks.member.controller;
 
+import com.ohgood.newstocks.member.dto.MemberDto;
 import com.ohgood.newstocks.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping({"", "/{memberId}"})
+    public ResponseEntity<MemberDto> findMemberProfile(@PathVariable(required = false) Long memberId) {
+        // 추후 Authentication 처리 예정
+        return new ResponseEntity<>(memberService.findMemberProfile(memberId, 5L), HttpStatus.OK);
+    }
 
 }
--- backend/newstocks/src/main/java/com/ohgood/newstocks/member/service/MemberService.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/member/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.ohgood.newstocks.member.service;
 
+import com.ohgood.newstocks.member.dto.MemberDto;
+import com.ohgood.newstocks.member.entity.Member;
+import com.ohgood.newstocks.member.mapper.MemberMapper;
 import com.ohgood.newstocks.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,4 +16,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public MemberDto findMemberProfile(Long findMemberId, Long myMemberId) {
+
+        // 내 정보 조회
+        if (findMemberId == null) {
+            return MemberMapper.INSTANCE.entityToMemberDto(findMemberById(myMemberId));
+        }
+        // 타인 정보 조회
+        // 추후에 프로필 공개 여부 추가할 수 있어 분리
+        return MemberMapper.INSTANCE.entityToMemberDto(findMemberById(findMemberId));
+    }
+
+    public Member findMemberById(Long userId) {
+        return memberRepository.findByIdAndDeletedFalse(userId)
+            .orElseThrow(() -> new ArithmeticException("해당하는 회원이 없습니다."));
+    }
 }
