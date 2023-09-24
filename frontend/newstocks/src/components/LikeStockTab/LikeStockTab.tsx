48cae2b020a76c9bd893fa0531fc393f2895274a
@iwri2089 <iwri2089@gmail.com>
2023-09-24 18:47:51 +0900

    :zap: feat: 관심종목 delete button 추가

--- frontend/newstocks/src/components/LikeStockTab/LikeStockTab.tsx
+++ frontend/newstocks/src/components/LikeStockTab/LikeStockTab.tsx
@@ -1,7 +1,8 @@
 "use client";
 import styles from "./LikeStocktab.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useSearchParams, useRouter } from "next/navigation";
 
 import SearchBox from "../SearchBox/SearchBox";
 import StockProfile from "../StockProfile/StockProfile";
@@ -14,29 +15,74 @@ import { BiXCircle } from "react-icons/bi";
 import axios from "axios";
 
 export default function LikeStockTab() {
+  const router = useRouter();
+  const searchParams = useSearchParams(); 
   const [addtoggle, setAddtoggle] = useState(false);
   const [allFavoriteStocks, setAllFavoriteStocks] = useState<Stock[]>([]);
+
+  const handleSelectStock = (id: string) => {
+
+    const tabName = searchParams?.get("tab");
+    router.push(`/${id}?tab=${tabName}`);
+
+  }
 
   const handleSearch = (stock: Stock) => {
     setAllFavoriteStocks((prev) => [...prev, stock]);
 
     // const addFavoriteStock = async (stock: Stock) => {
+    //   try {
+    //     const response = await axios.post(
+    //       "http://localhost:8200/stock/insert-favorite-stock",
+    //       {
+    //         stockId: stock.id,
+    //         stockName: stock.name,
+    //       },
+    //       {
+    //         headers: {
+    //           Authorization: "",
+    //         },
+    //       }
+    //     );
+    //     console.log(response);
+    //   } catch (e) {
+    //     console.error(e);
+    //   }
+    // };
 
-    //   const response = await axios.post("http://localhost:8200/stock/insert-favorite-stock")
+    // addFavoriteStock(stock);
+  };
 
-    // }
+  const handleDelete = async (id: string, name: string) => {
+    console.log("delete");
+    try {
+      const response = await axios.delete(
+        "http://localhost:8200/stock/delete-favorite-stock",
+        {
+          headers: {
+            Authorization: "",
+          },
+          data: {
+            stockId: id,
+            stockName: name,
+          },
+        }
+      );
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   const favoriteStocks = allFavoriteStocks.map(({ name, id }) => {
     return (
-      <div key={id} className={styles['like-content-stock']}>
+      <div key={id} className={styles["like-content-stock"]} onClick={()=>handleSelectStock(id)}>
         <StockProfile
           stockName={name}
           stockId={id}
           stockImageUrl={`https://file.alphasquare.co.kr/media/images/stock_logo/kr/${id}.png`}
           size="small"
         />
-        <BiXCircle size="18" />
+        <BiXCircle size="18" onClick={handleDelete} id={styles["delete-button"]} />
       </div>
     );
   });
