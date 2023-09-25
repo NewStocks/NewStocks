8d946b97874041874b519d81f8fbe0528fcbad22
김민석 <kmin9811@naver.com>
2023-09-25 15:16:03 +0900

    :sparkles: feat: find follower/following List API created

--- backend/newstocks/src/main/java/com/ohgood/newstocks/member/controller/FollowController.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/member/controller/FollowController.java
@@ -1,11 +1,14 @@
 package com.ohgood.newstocks.member.controller;
 
+import com.ohgood.newstocks.member.dto.MemberDto;
 import com.ohgood.newstocks.member.service.FollowService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowController {
 
     private final FollowService followService;
+
+    @GetMapping("/follower")
+    public ResponseEntity<List<MemberDto>> findFollowerList() {
+        // 추후 Authentication 처리 예정
+        return new ResponseEntity<>(followService.findFollowerList(5L), HttpStatus.OK);
+    }
+
+    @GetMapping("/following")
+    public ResponseEntity<List<MemberDto>> findFollowingList() {
+        // 추후 Authentication 처리 예정
+        return new ResponseEntity<>(followService.findFollowingList(5L), HttpStatus.OK);
+    }
 
     @PostMapping("/{follwingId}")
     public ResponseEntity<Void> follow(@PathVariable Long follwingId) {
--- backend/newstocks/src/main/java/com/ohgood/newstocks/member/service/FollowService.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/member/service/FollowService.java
@@ -1,10 +1,13 @@
 package com.ohgood.newstocks.member.service;
 
 import com.ohgood.newstocks.global.exception.exceptions.BadRequestException;
+import com.ohgood.newstocks.member.dto.MemberDto;
 import com.ohgood.newstocks.member.entity.Follow;
 import com.ohgood.newstocks.member.entity.Member;
+import com.ohgood.newstocks.member.mapper.MemberMapper;
 import com.ohgood.newstocks.member.repository.FollowRepository;
 import com.ohgood.newstocks.member.repository.MemberRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,22 @@ public class FollowService {
 
     private final FollowRepository followRepository;
     private final MemberRepository memberRepository;
+
+    public List<MemberDto> findFollowerList(Long followingId) {
+        List<Long> followerIdList = followRepository.findByFollowingId(followingId).stream()
+            .map(Follow::getFollowerId).toList();
+
+        return memberRepository.findByIdInAndDeletedFalse(followerIdList).stream()
+            .map(MemberMapper.INSTANCE::entityToMemberDto).toList();
+    }
+
+    public List<MemberDto> findFollowingList(Long followerId) {
+        List<Long> followingIdList = followRepository.findByFollowerId(followerId).stream()
+            .map(Follow::getFollowingId).toList();
+
+        return memberRepository.findByIdInAndDeletedFalse(followingIdList).stream()
+            .map(MemberMapper.INSTANCE::entityToMemberDto).toList();
+    }
 
     @Transactional
     public void follow(Long followerId, Long followingId) {
