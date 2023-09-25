95c567f42d9cda15415debffa69b7eb6b05134f6
1004joonsuk <1004joonsuk@naver.com>
2023-09-25 10:46:47 +0900

    :zap: feat: 데이터 삽입 후 메서드 무효화, 나의 관심 종목 리스트 조회 메서드 개발

--- backend/newstocks/src/main/java/com/ohgood/newstocks/stock/controller/StockController.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/stock/controller/StockController.java
@@ -2,31 +2,26 @@ package com.ohgood.newstocks.stock.controller;
 
 import com.ohgood.newstocks.auth.jwt.service.JwtService;
 import com.ohgood.newstocks.stock.dto.ChartResDto;
+import com.ohgood.newstocks.stock.dto.FavoriteStockDto;
 import com.ohgood.newstocks.stock.dto.FavoriteStockReqDto;
 import com.ohgood.newstocks.stock.dto.StockResDto;
 import com.ohgood.newstocks.stock.dto.StockSearchDto;
-import com.ohgood.newstocks.stock.dto.ValueChainDto;
 import com.ohgood.newstocks.stock.dto.ValueChainResDto;
 import com.ohgood.newstocks.stock.service.StockService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO: 이거 나중에 데이터 다 넣고 나서 RestController로 다시 되돌려 놔야함
-//@RestController
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/stock")
 public class StockController {
@@ -57,9 +52,7 @@ public class StockController {
     @PostMapping("/insert-favorite-stock")
     public ResponseEntity<String> insertFavoriteStock(
         @RequestBody FavoriteStockReqDto favoriteStockReqDto) {
-//        , @RequestHeader("access-token") String id) {
-//        System.out.println(id);
-//        Long memberId = Long.parseLong(id);
+//        authentication.getName();
         System.out.println(favoriteStockReqDto.toString());
         return new ResponseEntity<>(stockService.insertFavoriteStock(favoriteStockReqDto, 1L),
             HttpStatus.OK);
@@ -68,8 +61,14 @@ public class StockController {
     @DeleteMapping("/delete-favorite-stock")
     public ResponseEntity<String> deleteFavoriteStock(
         @RequestBody FavoriteStockReqDto favoriteStockReqDto) {
-//        @RequestHeader("access-token") Long id) {
         return new ResponseEntity<>(stockService.deleteFavoriteStock(favoriteStockReqDto, 1L),
+            HttpStatus.OK);
+    }
+
+    @GetMapping("find-favorite-stock-by-member-id/{member-id}")
+    public ResponseEntity<List<FavoriteStockDto>> findFavoriteStockByMemberId(
+        @PathVariable("member-id") Long memberId) {
+        return new ResponseEntity<>(stockService.findAllFavoriteStockByMemberId(memberId),
             HttpStatus.OK);
     }
 
@@ -80,23 +79,20 @@ public class StockController {
             HttpStatus.OK);
     }
 
-    //데이터 삽입용 임시페이지
-    @GetMapping("/value-chain-insert")
-    public String showInsertPage() {
-        // /value-chain-insert 경로로 요청이 들어오면 insert 페이지를 표시
-        return "insertpage";
-    }
-
-    @PostMapping("/save-value-chain")
-    public String saveValueChain(
-        @RequestParam("stock_id") String stockId,
-        @RequestParam("value_chain_id") String valueChainId,
-        @RequestParam("value_chain_name") String valueChainName,
-        Model model) {
-
-        String isSuccess = stockService.saveValueChain(stockId, valueChainId, valueChainName);
-        model.addAttribute("message", isSuccess);
-
-        return "insertpage";
-    }
+//    데이터 삽입용 임시페이지
+//    @GetMapping("/value-chain-insert")
+//    public String showInsertPage() {
+//        return "insertpage";
+//    }
+//
+//    @PostMapping("/save-value-chain")
+//    public String saveValueChain(
+//        @RequestParam("stock_id") String stockId,
+//        @RequestParam("value_chain_id") String valueChainId,
+//        @RequestParam("value_chain_name") String valueChainName,
+//        Model model) {
+//        String isSuccess = stockService.saveValueChain(stockId, valueChainId, valueChainName);
+//        model.addAttribute("message", isSuccess);
+//        return "insertpage";
+//    }
 }
--- backend/newstocks/src/main/java/com/ohgood/newstocks/stock/dto/FavoriteStockDto.java
+++ backend/newstocks/src/main/java/com/ohgood/newstocks/stock/dto/FavoriteStockDto.java
@@ -1,5 +1,6 @@
 package com.ohgood.newstocks.stock.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ohgood.newstocks.member.entity.Member;
 import lombok.Builder;
 import lombok.Data;
@@ -7,12 +8,14 @@ import lombok.Data;
 @Data
 public class FavoriteStockDto {
 
+    @JsonIgnore
     private Member member;
     private String stockId;
     private String stockName;
 
     @Builder
-    public FavoriteStockDto(String stockId, String stockName) {
+    public FavoriteStockDto(Member member, String stockId, String stockName) {
+        this.member = member;
         this.stockId = stockId;
         this.stockName = stockName;
     }
