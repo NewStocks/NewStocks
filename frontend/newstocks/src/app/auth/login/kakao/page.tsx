a67a526f972525978eb2bc8a996b31ef40ec1512
paengjiwoo <aprilzu0427@gmail.com>
2023-09-26 13:12:19 +0900

    :lipstick: style: 커뮤니티 Card 반응형 스타일 수정중

--- frontend/newstocks/src/app/auth/login/kakao/page.tsx
+++ frontend/newstocks/src/app/auth/login/kakao/page.tsx
@@ -1,23 +1,26 @@
 'use client'
 import { useEffect } from 'react';
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 import axios from 'axios';
 
 export default function KakaoLogin() {
   // const code = window.location.search;
-  const code = new URL(document.location.toString()).searchParams.get('code');
-  console.log('code', code)
   useEffect(() => {
-    axios({
-      method: 'post',
-      url: `http://localhost:8200/auth/login/kakao`,
-      data: { code }})
-      .then((res) => {
-        console.log(res.data);
-        // localStorage.setItem('token', res.data?._token)
-        // redirect('/')
-      })
-  })
+    const code = new URL(document.location.toString()).searchParams.get('code');
+    console.log('code', code)
+    if (code) {
+      axios({
+        method: 'post',
+        url: `http://localhost:8200/auth/login/kakao`,
+        data: { code }})
+        .then((res) => {
+          console.log(res);
+          localStorage.setItem('token' , res.data?.memberDto.id)
+          window.location.href = '/'
+        })
+      }
+    })
+
   return <div>로그인 중입니다.</div>;
 }
 
